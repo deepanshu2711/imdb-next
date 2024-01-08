cd43ace8f679689dec01147a0c9022dc61446ad7
deepanshu2711 <deepanshusaini2711@gmail.com>
2024-01-08 18:53:44 +0530

    [FEAT] Add search box

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,10 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
-import Header from '@/components/Header'
+import Header from '../components/Header'
+import NavBar from '../components/NavBar'
 import Providers from './Providers'
-import NavBar from '@/components/Navbar'
+
+import SearchBox from '../components/SearchBox'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,6 +20,7 @@ export default function RootLayout({ children }) {
       <Providers>
       <Header />
       <NavBar />
+      <SearchBox />
       {children}
       </Providers>
       </body>
--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,4 @@
-import Results from "@/components/Results";
+import Results from "../components/Results";
 import { Result } from "postcss";
 
 
