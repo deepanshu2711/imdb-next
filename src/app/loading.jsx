2026ef644eae2e22f4cf6df8ea6b05bc4a932575
deepanshu2711 <deepanshusaini2711@gmail.com>
2024-01-08 15:28:26 +0530

    [FEAT] add loading effect

---
+++ src/app/loading.jsx
@@ -0,0 +1,9 @@
+const loading = () => {
+    return ( 
+        <div className="flex justify-center">
+            <img src="spinner.svg" alt="loading" className="h-52"/>
+        </div>
+     );
+}
+ 
+export default loading; 
--- src/app/page.js
+++ src/app/page.js
@@ -12,7 +12,8 @@ export default async function Home({searchParams}) {
   const res = await fetch(
     `https://api.themoviedb.org/3${
       genre === 'fetchTopRated' ? `/movie/top_rated` : `/trending/all/week`
-    }?api_key=${API_KEY}&language=en-US&page=1`
+    }?api_key=${API_KEY}&language=en-US&page=1`,
+    {next:{revalidate:10000}}
   );
 
   const data = await res.json();
@@ -30,6 +31,7 @@ export default async function Home({searchParams}) {
             <Results key={res.id} title={res.original_title} />
         ))
       }
+      
     </div>
   )
 }
